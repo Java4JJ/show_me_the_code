032cf55b7e14a3b5fe914ae72ccfe5230452a1b2
mt <mt>
2024-08-13 15:44:42 +0800

    新增leetcode题目

---
+++ leetcode_top100/implement-queue-using-stacks/MyQueueQ.java
@@ -0,0 +1,41 @@
+class MyQueueQ {
+    /**
+     * 栈是先近后出的一种数据结果
+     * 队列是先进先出的一种数据结果
+     */
+
+    public MyQueueQ() {
+
+    }
+
+    /**
+     * void push(int x) 将元素 x 推到队列的末尾
+     *
+     * @param x
+     */
+    public void push(int x) {
+
+    }
+
+    /**
+     * int pop() 从队列的开头移除并返回元素
+     *
+     * @return
+     */
+    public int pop() {
+        return 0;
+    }
+
+    /**
+     * int peek() 返回队列开头的元素
+     *
+     * @return
+     */
+    public int peek() {
+        return 0;
+    }
+
+    public boolean empty() {
+        return false;
+    }
+}
---
+++ leetcode_top100/implement-stack-using-queues/MyStackQ.java
@@ -0,0 +1,27 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class MyStackQ {
+
+
+    public MyStackQ() {
+
+    }
+
+
+    public void push(int x) {
+
+    }
+
+    public int pop() {
+        return 0;
+    }
+
+    public int top() {
+        return 0;
+    }
+
+    public boolean empty() {
+        return false;
+    }
+}
