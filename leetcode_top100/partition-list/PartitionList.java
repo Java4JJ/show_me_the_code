8e4feca99885949159f1ac6fc22cc4fa13384455
Java4JJ <Java4JJ@163.com>
2024-08-15 17:27:57 +0800

    复习分隔链表

--- leetcode_top100/partition-list/PartitionList.java
+++ leetcode_top100/partition-list/PartitionList.java
@@ -20,46 +20,6 @@ public class PartitionList {
      * @return
      */
     public ListNode partition(ListNode head, int x) {
-        //先确定4个范围
-        //left表示小于x节点的头节点和尾节点
-        ListNode leftHead = null, leftTail = null;
-        //right表示大于等于x节点的头结点和尾节点
-        ListNode rightHead = null, rightTail = null;
-
-        //next变量用来存储下一个节点
-        ListNode next = null;
-
-        while (head != null) {
-            next = head.next;
-            //断开head.next的指针
-            head.next = null;
-            if (head.val < x) {
-                //设置小于x区域的头和尾，头只需要设置第一次
-                if (leftHead == null) {
-                    leftHead = head;
-                } else {
-                    leftTail.next = head;
-                }
-                leftTail = head;
-            } else {
-                //设置大于x区域的头和尾，头只需要设置第一次
-                if (rightHead == null) {
-                    rightHead = head;
-                } else {
-                    rightTail.next = head;
-                }
-                rightTail = head;
-            }
-            //保证循环，head往下跳一步
-            head = next;
-        }
-
-        //如果没有小于x的区域，那么可以直接返回大于x的区域
-        if (leftHead == null) {
-            return rightHead;
-        }
-        //小于x区域的尾指向大于x区域的头
-        leftTail.next = rightHead;
-        return leftHead;
+        return null;
     }
 }
